dfb0bb9440965a6664208752e0a3d1a0d6bbba25
dn667-ux <dn6674990@gmail.com>
2026-02-03 17:20:15 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+WEBHOOK_URL = os.getenv("https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU")
+
+if not WEBHOOK_URL:
+    raise Exception("https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU not set")
+
+requests.post(
+    WEBHOOK_URL,
+    json={"content": "HELLO"}
+)
+
+print("Message sent")
