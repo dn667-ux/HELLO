80834eb979c7523b86c91ae80e2fd20954a6daa5
dn667-ux <dn6674990@gmail.com>
2026-02-03 17:23:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 WEBHOOK_URL = os.getenv("https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU")
 
 if not WEBHOOK_URL:
-    raise Exception("https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU not set")
+    raise Exception("DISCORD_WEBHOOK not set")
 
 requests.post(
     WEBHOOK_URL,
