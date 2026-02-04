9f5942d24da96bde9ef8691cb385009d382151a0
dn667-ux <dn6674990@gmail.com>
2026-02-03 17:32:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
-import os
 
-WEBHOOK_URL = os.getenv("https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU")
+WEBHOOK_URL = "https://discord.com/api/webhooks/1468337671041581168/vII1Ke1qLMf_Cus3D7j51puPUShPTNU574Zs7E5VxmGEIQ4qT5Eyx9KVfHDNCZpcFvvU"
 
-if not WEBHOOK_URL:
-    raise Exception("DISCORD_WEBHOOK not set")
+def send_message():
+    data = {
+        "content": "HELLO, testing"
+    }
+    response = requests.post(WEBHOOK_URL, json=data)
+    if response.status_code == 204:
+        print("Message sent successfully!")
+    else:
+        print("Failed to send message:", response.text)
 
-requests.post(
-    WEBHOOK_URL,
-    json={"content": "HELLO"}
-)
-
-print("Message sent")
+if __name__ == "__main__":
+    send_message()
